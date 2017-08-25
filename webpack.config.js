a8d16298ca02901008161b2548634d1f5246d9a7
Diego Leal <leal.banks@gmail.com>
2017-08-25 20:46:27 -0300

    [UPDATE]: O entry file foi renomeado

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require( 'path' );
 
 module.exports = {
-  entry: path.resolve( 'app/src/entry.js' ),
+  entry: path.resolve( 'app/src/index.js' ),
   output: {
     filename: 'bundle.js',
     path: path.resolve( 'app/dist')
