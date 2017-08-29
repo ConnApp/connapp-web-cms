2c2de4b2f16f1f663ddf1a2a5019f2ca8152c573
Diego Leal <leal.banks@gmail.com>
2017-08-29 17:06:20 -0300

    [NEW]: Adicionado model de usuário.

---
+++ api/models/user.js
@@ -0,0 +1,58 @@
+const
+    mongoose = require( 'mongoose' )
+    Schema = mongoose.Schema;
+
+module.exports = function() {
+  const
+    collectionName = 'user',
+    userSchema;
+
+  userSchema = new Schema({
+    /**
+    * Primeiro nome do usuário.
+    * @type {String}
+    */
+    firstName: {
+      type: String,
+      required: true
+    },
+    /**
+    * Sobrenome do usuário.
+    * @type {String}
+    */
+    lastName: {
+      type: String,
+      required: true
+    },
+    /**
+    * Email do usuário.
+    * @type {String}
+    */
+    email: {
+      type: String,
+      required: true,
+      index: {
+        unique: true
+      }
+    },
+    /**
+    * Senha do usuário.
+    * @type {String}
+    */
+    password: {
+      type: String,
+      required: true
+    },
+    /**
+    * Nível de acesso do usuário.
+    * @type {String}
+    */
+    group: {
+      type: String,
+      enum: [ 'user', 'admin' ],
+      required: true
+    }
+  });
+
+  return mongoose.model( collectionName, userSchema );
+}
