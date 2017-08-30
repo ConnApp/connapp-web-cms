1f2d408bdf162305d5fbbcd6eb91434f3e1bdc7a
Diego Leal <leal.banks@gmail.com>
2017-08-30 17:35:11 -0300

    Adicionado lib para manipular querys.

---
+++ api/lib/mongoose.js
@@ -0,0 +1,46 @@
+const mongoose = require( 'mongoose' );
+
+module.exports = function( Model ) {
+  // if ( !( Model instanceof mongoose.model ) ) throw new Error( 'Invalid mongoose model ' );
+
+  function get( query = {} ) {
+    return Model.findOne( query );
+  }
+
+  function query( query = {} ) {
+    return Model.find( query );
+  }
+
+  function save( doc ) {
+    let _doc;
+
+    try {
+      _doc = new Model( doc );
+      return _doc.save();
+    } catch( error ) {
+      return new Promise(( resolve, reject ) => reject( error ));
+    }
+
+  }
+
+  function update( query = {}, doc, options = { multi: false } ) {
+    try {
+      return Model.update( query, doc, options );
+    } catch( error ) {
+      return new Promise(( resolve, reject ) => reject( error ));
+    }
+
+  }
+
+  function logicalRemove( query ) {
+    return Model.update( query, { $set: { active: false } } );
+  }
+
+  return {
+    get,
+    query,
+    save,
+    update,
+    logicalRemove
+  };
+};
