9f884d060090da50d3fc89574be53658ad4cecb7
Diego Leal <leal.banks@gmail.com>
2017-09-05 09:52:22 -0300

    [UPDATE]: Adicionado teste simples para os endpoint /list e /remove.

--- api/test/unit/user.spec.js
+++ api/test/unit/user.spec.js
@@ -1,11 +1,8 @@
 const
   request = require( 'superagent' ),
   path = require( 'path' ),
-  HOST = 'http://localhost:3000';
-
-
-test( 'Teste para o endpoint /user/new', done => {
-  const user =  {
+  HOST = 'http://localhost:3000',
+  user =  {
     firstName: 'John',
     lastName: 'Doe',
     email: 'johndoe@email.com',
@@ -13,13 +10,40 @@ test( 'Teste para o endpoint /user/new', done => {
     group: 'admin'
   };
 
+
+test( 'Teste simples para o endpoint /user/new', done => {
   request
     .post( `${ HOST }/user/new` )
     .send( user )
     .set( 'Accept', 'application/json' )
     .end(( error, res ) => {
-      if ( error ) throw error;
+      expect( error ).toBeFalsy();
+      done();
+    });
 
+});
+
+test( 'Teste simples para o endpoint /user/list', done => {
+  request
+    .get( `${ HOST }/user/list` )
+    .set( 'Accept', 'application/json' )
+    .end(( error, res ) => {
+      console.log( res.text );
+      console.log( res.text.length );
+      expect( error ).toBeFalsy();
+      // expect( res.text.length ).toHaveLength( 1 );
+      done();
+    });
+});
+
+test( 'Teste simples para o endpoint /user/remove/:email', done => {
+  request
+    .get( `${ HOST }/user/remove/${ user.email }` )
+    .set( 'Accept', 'application/json' )
+    .end(( error, res ) => {
+      expect( error ).toBeFalsy();
+      console.log( res.text );
+      // expect( res.text.length ).toHaveLength( 1 );
       done();
     });
 });
