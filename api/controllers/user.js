6020d5af173eb6befd4f2082d48993ebe603c497
Diego Leal <leal.banks@gmail.com>
2017-09-05 09:51:38 -0300

    [UPDATE]:  - Adicionado comentários para jsdoc;  - Adicionado função para transformar o password em um hash;  - Adicionado método query;  - Adicionado método remove.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,9 +1,34 @@
+const bcrypt = require( 'bcrypt' );
+
 module.exports = function( app ) {
   const
     Model = app.models.user,
     user = require( '../lib/mongoose')( Model );
 
-  function createUser( req, res ) {
+  /**
+  * Responde a requisição com status de sucesso e envia um json com os dados do usuário.
+  * @memberof User
+  * @type {Method} - private
+  * @param {Object} _doc - Dados do usuário
+  */
+  function _resolveResponse( res, data ) {
+    res.status( 200 ).json( data );
+  }
+  /**
+  * Responde a requisição com status de error e envia um json com o objeto do erro.
+  * @memberof User
+  * @type {Method} - private
+  * @param {Object} error - Objeto com messagem e stack de erro.
+  */
+  function _rejectResponse( res, error ) {
+    res.status( 500 ).json( error );
+  }
+  /**
+  * Cria um nove usuário.
+  * @memberof User
+  * @method POST
+  */
+  function create( req, res ) {
     let doc = req.body;
 
     Promise.resolve( doc )
@@ -11,17 +36,31 @@ module.exports = function( app ) {
       .then( validateGroup )
       .then( validatePassword )
       .then( validateEmail )
+      .then( makeHashWithPass )
       .then( save )
-      .then( resolveResponse )
-      .catch( rejectResponse );
+      .then( _resolveResponse.bind( null, res ) )
+      .catch( _rejectResponse.bind( null, res ) );
 
+    /**
+    * Verifica se o nome e sobrenome foi enviado.
+    * @memberof User.create
+    * @type {Method}
+    * @param {Object} _doc - Dados do usuário
+    * @return {Object} _doc - Dados do usuário
+    */
     function validateName( _doc ) {
       if ( !( _doc.firstName || _doc.lastName ) ) {
         throw new Error( 'O nome do usuário deve ser informado.' );
       }
       return _doc;
     }
-
+    /**
+    * Verifica se o grupo de usuário inforamdo é válido.
+    * @memberof User.create
+    * @type {Method}
+    * @param {Object} _doc - Dados do usuário
+    * @return {Object} _doc - Dados do usuário
+    */
     function validateGroup( _doc ) {
       const
         groups = [ 'admin', 'user' ],
@@ -34,7 +73,13 @@ module.exports = function( app ) {
 
       return _doc;
     }
-
+    /**
+    * Verifica na base de dados se o email já está sendo utilizado por outro usuário.
+    * @memberof User.create
+    * @type {Method}
+    * @param {Object} _doc - Dados do usuário
+    * @return {Object} _doc - Dados do usuário
+    */
     function validateEmail( _doc ) {
       return new Promise( ( resolve, reject ) => {
         user.get( { email: _doc.email } )
@@ -47,33 +92,80 @@ module.exports = function( app ) {
         });
       });
     }
-
+    /**
+    * Verifica se a senha possuí no minimo 8 caracteres
+    * @memberof User.create
+    * @type {Method}
+    * @param {Object} _doc - Dados do usuário
+    * @return {Object} _doc - Dados do usuário
+    */
     function validatePassword( _doc ) {
       if ( !_doc.password || _doc.password.length < 8 ) {
         throw new Error( 'A senha não pode está vazia ou ser menor que 8 caracteres' );
       }
       return _doc;
     }
-
+    /**
+    * Gera uma criptografia da senha informada.
+    * @memberof User.create
+    * @type {Method}
+    * @param {Object} _doc - Dados do usuário
+    * @return {Object} _doc - Dados do usuário
+    */
+    function makeHashWithPass( _doc ) {
+      const salt = 10;
+      _doc.password = bcrypt.hashSync( _doc.password, salt );
+      return _doc;
+    }
+    /**
+    * Armazena os dados do usuário no banco de dados.
+    * @memberof User.create
+    * @type {Method}
+    * @param {Object} _doc - Dados do usuário
+    * @return {Object} _doc - Dados do usuário
+    */
     function save( _doc ) {
       return new Promise( ( resolve, reject ) => {
         user.save( _doc )
-          .then( _user => resolve( _user ) )
-          .catch( error => reject( error ) );
+          .then( _user => resolve( _user ) );
       });
     }
+  }
+  /**
+  * Busca todos os usuários da base de dados.
+  * @memberof User
+  * @method GET
+  */
+  function list( req, res ) {
+    Promise.resolve()
+      .then( query )
+      .then( _resolveResponse.bind( null, res ) )
+      .catch( _rejectResponse.bind( null, res ) );
 
-    function resolveResponse( _user ) {
-      res.status( 200 ).json( _user );
+    /**
+    * Busca todos os usuários do banco de dados.
+    * @memberof User.list
+    * @return {Promise} Promise - Promise resolvida com um array de usuários.
+    */
+    function query() {
+      return new Promise( ( resolve, reject ) => user.query( {} ).then( _users => resolve( _users ) ) );
     }
-
-    function rejectResponse( error ) {
-      console.log( error );
-      res.status( 500 ).json( error );
-    }
+  }
+  /**
+  * Desativa o usuário.
+  * @memberof User
+  * @method GET
+  */
+  function remove( req, res ) {
+    const email = req.params;
+    Model.remove( { email: email } )
+      .then( status => _resolveResponse.bind( null, res ) )
+      .catch( error => _rejectResponse.bind( null, res ) );
   }
 
   return {
-    createUser
+    create,
+    list,
+    remove
   };
 };
