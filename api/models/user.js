353cf451d1ffb3d4e18647e3b94ff311879e05eb
Diego Leal <leal.banks@gmail.com>
2017-09-07 21:38:30 -0300

    [UPDATE]: Adicionado três novas propriedades: active, lastUpdate e createAt.

--- api/models/user.js
+++ api/models/user.js
@@ -1,56 +1,80 @@
 const
-    mongoose = require( 'mongoose' ),
-    Schema = mongoose.Schema;
+  mongoose = require( 'mongoose' ),
+  Schema = mongoose.Schema;
 
 module.exports = function() {
   const
     collectionName = 'user',
     userSchema = new Schema({
-    /**
-    * Primeiro nome do usuário.
-    * @type {String}
-    */
-    firstName: {
-      type: String,
-      required: true
-    },
-    /**
-    * Sobrenome do usuário.
-    * @type {String}
-    */
-    lastName: {
-      type: String,
-      required: true
-    },
-    /**
-    * Email do usuário.
-    * @type {String}
-    */
-    email: {
-      type: String,
-      required: true,
-      index: {
-        unique: true
+      /**
+      * Primeiro nome do usuário.
+      * @type {String}
+      */
+      firstName: {
+        type: String,
+        required: true
+      },
+      /**
+      * Sobrenome do usuário.
+      * @type {String}
+      */
+      lastName: {
+        type: String,
+        required: true
+      },
+      /**
+      * Email do usuário.
+      * @type {String}
+      */
+      email: {
+        type: String,
+        required: true,
+        index: {
+          unique: true
+        }
+      },
+      /**
+      * Senha do usuário.
+      * @type {String}
+      */
+      password: {
+        type: String,
+        required: true
+      },
+      /**
+      * Nível de acesso do usuário.
+      * @type {String}
+      */
+      group: {
+        type: String,
+        enum: [ 'user', 'admin' ],
+        default: 'user'
+      },
+      /**
+      * Define se o usuário está ativo.
+      * @type {Boolean}
+      */
+      active: {
+        type: Boolean,
+        default: true
+      },
+      /**
+      * Data de criação do usuário.
+      * @type {Date}
+      */
+      createAt: {
+        type: Date,
+        default: Date.now
+      },
+      /**
+      * Data da última vez que o usuário foi atualizado.
+      * @type {String}
+      */
+      lastUpdate: {
+        type: Date,
+        default: Date.now
       }
-    },
-    /**
-    * Senha do usuário.
-    * @type {String}
-    */
-    password: {
-      type: String,
-      required: true
-    },
-    /**
-    * Nível de acesso do usuário.
-    * @type {String}
-    */
-    group: {
-      type: String,
-      enum: [ 'user', 'admin' ],
-      default: 'user'
-    }
-  }, { collection: collectionName } );
+    }, { collection: collectionName } );
 
   return mongoose.model( collectionName, userSchema );
-}
+};
