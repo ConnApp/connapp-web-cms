c0f09ffe89472cd07ac91ffa28ffe07bab023abc
Diego Leal <leal.banks@gmail.com>
2017-09-07 21:37:37 -0300

    [UPDATE]: Adicionado endpoints update e disable.

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,9 @@ module.exports = function( app ) {
   const userController = app.controllers.user;
 
   app.post( '/user/new', userController.create );
+  app.post( '/user/update', userController.update );
   app.get( '/user/list', userController.list );
+  app.get( '/user/disable/:email', userController.disable );
   app.get( '/user/remove/:email', userController.remove );
-}
+  
+};
