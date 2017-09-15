ee7fcd7ae11b1b7d154593072abe837e6dd26628
Diego Leal <leal.banks@gmail.com>
2017-09-15 18:43:57 -0300

    [UPDATE]: Adicionado chamada da função signin.

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,9 @@
+
 module.exports = function( app ) {
   const authController = app.controllers.auth;
 
   app.get( '/auth/signin', authController.renderLogin );
-  app.post( '/auth/signin', authController.signin );
+  app.post( '/auth/signin', authController.signin() );
   app.get( '/auth/signout', authController.signout );
   
 };
