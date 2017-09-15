fb17dd49539b09d4c8ba6e8e88a30c666a1adb06
Diego Leal <leal.banks@gmail.com>
2017-09-15 11:46:05 -0300

    [UPDATE]: Adicionado crud para tipo de eventos.

---
+++ api/models/eventType.js
@@ -0,0 +1,44 @@
+const
+  mongoose = require( 'mongoose' ),
+  Schema = mongoose.Schema;
+
+module.exports = function() {
+  const
+    collectionName = 'eventType',
+    userSchema = new Schema({
+      /**
+      * Nome do eventType.
+      * @type {String}
+      */
+      name: {
+        type: String,
+        required: true
+      },
+      /**
+      * Define se o eventType está ativo.
+      * @type {Boolean}
+      */
+      active: {
+        type: Boolean,
+        default: true
+      },
+      /**
+      * Data de criação do eventType.
+      * @type {Date}
+      */
+      createAt: {
+        type: Date,
+        default: Date.now
+      },
+      /**
+      * Data da última vez que os dados do eventType foi atualizado.
+      * @type {String}
+      */
+      lastUpdate: {
+        type: Date,
+        default: Date.now
+      }
+    }, { collection: collectionName } );
+
+  return mongoose.model( collectionName, userSchema );
+};
