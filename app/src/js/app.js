0798b6c0f74503e8cc9598f427024833daaf56d5
Diego Leal <leal.banks@gmail.com>
2017-09-18 16:14:04 -0300

    [UPDATE]: Add angular resource module.

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,7 +1,8 @@
 import angular from 'angular';
 import angularRoute from 'angular-route';
+import angularResource from 'angular-resource';
 
 const APP_NAME = 'app';
-angular.module( APP_NAME, [ 'ngRoute' ] );
+angular.module( APP_NAME, [ 'ngRoute', 'ngResource' ] );
 
 export default APP_NAME;
