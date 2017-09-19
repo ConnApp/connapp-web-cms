3314dfbd5c8aa49bb4c7e0479b8c7d321d65d24b
Diego Leal <leal.banks@gmail.com>
2017-09-19 17:43:59 -0300

    [UPDATE]: Adicionado metodos getCurrentUser, setCurrentUser e redirectToForm.

--- app/src/js/controllers/app.user.js
+++ app/src/js/controllers/app.user.js
@@ -10,13 +10,19 @@
   * Controller para criar novos usuários
   * @memberof app.user
   */
-  function newUser( $log, $scope, $resource ) {
+  function newUser( $log, $scope, $resource, $routeParams ) {
     const 
       vm = this,
-      UserResource = $resource( '/users' );
+      { _id } = $routeParams,
+      UserResource = $resource( '/users', null,  { update: { method: 'PUT' } } );
     
     // ===# View Model #=== //
+    vm.user = {};
     vm.submitForm = submitForm;
+
+    if ( _id ) {
+      getUser( _id );
+    }
 
     function submitForm( user ) {
       const userResource = new UserResource();
@@ -29,23 +35,68 @@
           });
       }
     }
+
+    function getUser( _id ) {
+      const UserResource = $resource( '/users/:_id' );
+
+      UserResource.get( { _id } )
+        .$promise.then( user => {
+          angular.merge( vm.user, user );
+        })
+        .catch( $log.error );
+    }
   }
   /**
    * Controller para listar todos os usuários ativos.
    * @memberof app.user
    */
-  function listUser( $log, $resource ) {
+  function listUser( $log, $resource, $location ) {
     const 
       vm = this,
+      user = {},
       userResource = $resource( '/users' );
     
+    // ===# View models #=== //
     vm.orderBy = 'firstName';
+    vm.setCurrentUser = setCurrentUser;
+    vm.redirectToForm = redirectToForm;
 
     // ===# Carga inicial #=== //
     vm.users = listUsers();
 
+    /**
+     * Função realiza um requisição no endpoint /users e retorna uma lista de usuários
+     * @method GET
+     * @type {Function}
+     * @return {Array}
+     */
     function listUsers() {
       return userResource.query();
+    }
+    /**
+     * Função define um usuário global.
+     * @type {Function}
+     * @param {Object} user
+     */
+    function setCurrentUser( _user ) {
+      angular.merge( user, _user );
+    }
+    /**
+     * Função retorna um usuário
+     * @type {Function}
+     * @private
+     * @return {Object} vm.user
+     */
+    function getCurrentUser() {
+      return user;
+    }
+    /**
+     * Função redireciona para o formulário de usuários.
+     * @type {Function} 
+     */
+    function redirectToForm() {
+      const { _id } = getCurrentUser();
+      $location.path( `/user/form/${ _id }` );
     }
   }
 
