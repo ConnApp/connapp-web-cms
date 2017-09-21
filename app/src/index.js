09315366042027fc962f4d656fe7e9302f0f6f18
Diego Leal <leal.banks@gmail.com>
2017-09-21 12:24:46 -0300

    [UPDATE]: Add controller para gerenciar sessão.

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 // ===# Angular imports #=== //
 import app from './js/app';
+import session from './js/controllers/app.session';
 import userController from './js/controllers/app.user';
 import routeProvider from './js/providers/ngRoute.routeProvider';
 import userResource from './js/services/app.userResource';
