d63e2dc3de626aa8560d3421540f0f74c7748147
Diego Leal <leal.banks@gmail.com>
2017-09-21 12:23:03 -0300

    [RENAME]

---
+++ app/src/js/controllers/app.session.js
@@ -0,0 +1,45 @@
+
+!( function( angular ) {
+  if ( !angular ) throw new ReferenceError( 'angularjs 1.5+ is required' );
+  angular.module( 'app' )
+    .controller( 'sessionController', sessionController );
+
+  function sessionController( $log, $scope, $location, $resource ) {
+    const 
+      vm = this,
+      URL = 'http://localhost:3000';
+
+    // ===# View models #=== //
+    vm.authenticate = authenticate;
+
+    function authenticate( auth ) {
+      if ( $scope.formLogin.$invalid ) return;
+      const
+        AuthResource = $resource( '/auth/signin' ),
+        authResource = new AuthResource( auth );
+
+      authResource.$save()
+        .then( user => {
+          Promise.resolve( user )
+            .then( serializeResponse )
+            .then( createSession )
+            .then( redirectToHome );
+        })
+        .catch( $log.error );
+    }
+
+    function createSession( user ) {
+      sessionStorage.setItem( 'user', user );
+      return user;
+    }
+
+    function serializeResponse( { _id, firstName, lastName, group, email } ) {
+      return JSON.stringify( { _id, firstName, lastName, group, email } );
+    }
+
+    function redirectToHome() {
+      $location.url( URL );
+    }
+
+  }
+})( angular );
