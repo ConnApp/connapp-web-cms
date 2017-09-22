cbf458a1ec0330a146721e569dab6aba5634ed01
Diego Leal <leal.banks@gmail.com>
2017-09-22 13:39:38 -0300

    [UPDATE]: Adicionado componente de alerta.

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -2,8 +2,9 @@ import angular from 'angular';
 import angularRoute from 'angular-route';
 import angularResource from 'angular-resource';
 import uiMessages from './directives/messages/messages';
+import uiAlert from './directives/alert/alert';
 
 const APP_NAME = 'app';
-angular.module( APP_NAME, [ 'ngRoute', 'ngResource', 'uiMessages' ] );
+angular.module( APP_NAME, [ 'ngRoute', 'ngResource', 'uiMessages', 'uiAlert' ] );
 
 export default APP_NAME;
