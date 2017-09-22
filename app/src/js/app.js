a3bcbf799d4481cbd5edfe743502af9a0d313341
Diego Leal <leal.banks@gmail.com>
2017-09-22 10:56:27 -0300

    [UPDATE]: Adicionado modulo uiMessages.

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,8 +1,9 @@
 import angular from 'angular';
 import angularRoute from 'angular-route';
 import angularResource from 'angular-resource';
+import uiMessages from './directives/messages/messages';
 
 const APP_NAME = 'app';
-angular.module( APP_NAME, [ 'ngRoute', 'ngResource' ] );
+angular.module( APP_NAME, [ 'ngRoute', 'ngResource', 'uiMessages' ] );
 
 export default APP_NAME;
