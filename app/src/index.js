ed07d627f55e2f2cce2307c7555de2d79a260482
Diego Leal <leal.banks@gmail.com>
2017-09-23 11:49:25 -0300

    [UPDATE]: Adicionado homeController.

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@
 import app from './js/app';
 import session from './js/controllers/app.session';
 import userController from './js/controllers/app.user';
+import homeController from './js/controllers/app.home';
 import routeProvider from './js/providers/ngRoute.routeProvider';
 import userResource from './js/services/app.userResource';
 import alertService from './js/services/uiAlert.alert';
---
+++ app/src/js/controllers/app.home.js
@@ -0,0 +1,19 @@
+
+!(function( angular ) {
+  if ( !angular ) throw new ReferenceError( 'angularjs 1.5+ is required' );
+  angular.module( 'app' )
+    .controller( 'homeController', homeController );
+
+  function homeController() {
+    const vm = this;
+
+    // ===# View Model #=== //
+    vm.user = desserializeSession();
+    vm.isAdmin = vm.user.group === 'admin';
+
+    function desserializeSession() {
+      const user = sessionStorage.getItem( 'user' );
+      return JSON.parse( user );
+    }
+  }
+})( angular );
