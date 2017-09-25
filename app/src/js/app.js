f038896f267b5fbfce21826c50e581ec6e693fdd
Diego Leal <leal.banks@gmail.com>
2017-09-24 22:40:46 -0300

    [UPDATE]: Adicionado modules angular-sanitize e wizMarkdown.

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
 import angularRoute from 'angular-route';
+import angularSanitize from 'angular-sanitize';
 import angularResource from 'angular-resource';
 import uiMessages from './directives/messages/messages';
 import uiAlert from './directives/alert/alert';
+import wizMarkdown from './lib/wiz-markdown/wizMarkdown/wizMarkdown';
 
 const APP_NAME = 'app';
-angular.module( APP_NAME, [ 'ngRoute', 'ngResource', 'uiMessages', 'uiAlert' ] );
+angular.module( APP_NAME, [ 'ngRoute', 'ngResource', 'uiMessages', 'uiAlert', 'wiz.markdown' ] );
 
 export default APP_NAME;
