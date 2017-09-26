48fba69b08b134deccb2856599fa542d8055792a
Diego Leal <leal.banks@gmail.com>
2017-09-26 14:42:28 -0300

    [UPDATE]: Adicionado propriedade limit no body parser.

--- api/config/express.js
+++ api/config/express.js
@@ -23,7 +23,7 @@ module.exports = (function() {
   app.set( 'port', PORT );
   
   // ===# Middlewares setup #=== //
-  app.use ( bodyParser.json() );
+  app.use ( bodyParser.json( { limit: '5mb' } ) );
   app.use( bodyParser.urlencoded( { extended: true } ) );
   app.use( session( { name: 'connapp', secret: 'super_secret', saveUninitialized: false, resave: false } ) );
   app.use( methodOverride( 'X-HTTP-Method' ) );
