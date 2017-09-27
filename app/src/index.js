0e78cd43b5b558a80b384379df020540ebfc30a6
Diego Leal <leal.banks@gmail.com>
2017-09-27 14:27:34 -0300

    [UPDATE]: import httpProvider.

--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 // ===# module imports #=== //
 import app from './js/app';
 // ===# controller imports #=== //
@@ -5,8 +7,11 @@ import session from './js/controllers/app.session';
 import homeController from './js/controllers/app.home';
 import userController from './js/controllers/app.user';
 import newsController from './js/controllers/app.news';
+// ===# interceptor imports #=== //
+import loadingInterceptor from './js/interceptors/app.loading';
 // ===# provider imports #=== //
 import routeProvider from './js/providers/ngRoute.routeProvider';
+import httpProvider from './js/providers/app.httpProvider';
 // ===# service imports #=== //
 import userResource from './js/services/app.userResource';
 import newsResource from './js/services/app.newsResource';
