7fa720d95a3be4be3a85b72539a7cdc6e9f69a17
Diego Leal <leal.banks@gmail.com>
2017-10-04 17:51:15 -0300

    [UPDATE]: Adicionado ngInfiniteScroll

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -4,11 +4,12 @@ import angular from 'angular';
 import angularRoute from 'angular-route';
 import angularSanitize from 'angular-sanitize';
 import angularResource from 'angular-resource';
+import infiniteScroll from 'ng-infinite-scroll';
 import uiMessages from './directives/messages/messages';
 import uiAlert from './directives/alert/alert';
 import wizMarkdown from './lib/wiz-markdown/wizMarkdown/wizMarkdown';
 
 const APP_NAME = 'app';
-angular.module( APP_NAME, [ 'ngRoute', 'ngResource', 'uiMessages', 'uiAlert', 'wiz.markdown', 'flow' ] );
+angular.module( APP_NAME, [ 'ngRoute', 'ngResource', 'uiMessages', 'uiAlert', 'wiz.markdown', 'flow', 'ngInfiniteScroll' ] );
 
 export default APP_NAME;
