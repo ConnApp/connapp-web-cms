7c1ba662e16d61077c9899c2e0b0792a2609e2cd
Diego Leal <leal.banks@gmail.com>
2017-10-08 11:42:11 -0300

    [NEW]: Adicionado script para automatizar a inicialização do serviço.

---
+++ startup.sh
@@ -0,0 +1,20 @@
+!#/bin/bash
+
+#Inicializando dados dos usuários
+echo "Criando usuários..."
+mongoimport -d connapp-web-cms -c user ./api/collections/user.json
+echo "Done."
+
+#Instalando dependências
+echo "Instalando pacotes..."
+yarn run setup
+echo "Done."
+
+#Gerando bundle
+echo "Gerando bundle..."
+yarn run build
+echo "Done."
+
+#Inicializando servidor
+echo "Inicializando serviço..."
+node index.js
