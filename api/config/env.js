c1b1409129e3c0788adebac66399e411325ceccb
Diego Leal <leal.banks@gmail.com>
2017-10-08 11:41:01 -0300

    [UPDATE]: DATABASE_URL agora é provida por meio de variavel de ambiente.

--- api/config/env.js
+++ api/config/env.js
@@ -1,7 +1,7 @@
 const
   PORT = process.env.PORT || 3000,
   ROOT_PATH = process.env.PWD,
-  DATABASE_URL = 'mongodb://super:admin@ds149763.mlab.com:49763/connapp-web-cms';
+  DATABASE_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/connapp-web-cms';
 
 module.exports = {
   PORT,
